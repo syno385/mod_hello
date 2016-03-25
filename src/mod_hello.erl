78ef20db9ce97220e6b4c28458bd73a96e7c19fb
syn385 <adex.project@gmail.com>
2016-03-25 16:44:06 +0100

    123

--- src/mod_hello.erl
+++ src/mod_hello.erl
@@ -10,9 +10,15 @@
 
 start(_Host, _Opts) ->
     ?INFO_MSG("Hello, ejabberd world!", []),
+     ejabberd_hooks:add(filter_packet, global, ?MODULE, filter_packet, 100),
     ok.
 
 stop(_Host) ->
+	ejabberd_hooks:delete(filter_packet, global, ?MODULE, filter_packet, 100),
     ?INFO_MSG("Bye bye, ejabberd world!", []),
     ok.
 
+
+
+filter_packet({From, To, Packet} = Input) ->
+end.
